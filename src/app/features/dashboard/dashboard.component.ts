ba7619b3385127db4e8ca29ebec101504ab80780
Leonardo <contato.leoo0@gmail.com>
2025-02-26 20:40:29 -0300

    feat: implements dashboard UI

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [NzButtonModule, NzIconModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
