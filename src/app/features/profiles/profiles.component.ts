465729148554d9db426cd87812c7f6402247f3d4
Leonardo <contato.leoo0@gmail.com>
2025-03-05 21:09:32 -0300

    refactor: muda o layout de `profiles` e padroniza espaçamentos

--- src/app/features/profiles/profiles.component.ts
+++ src/app/features/profiles/profiles.component.ts
@@ -1,46 +1,23 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzIconModule } from 'ng-zorro-antd/icon';
-import { LocalStorageService } from '../../../core/localstorage/local-storage.service';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { LocalStorageKeys } from '../../constants/local-storage-keys';
 import { CommonModule } from '@angular/common';
-import { DynamicFormComponent } from '../shared/components/dinamyc-form/dynamic-form.component';
+import { NzCardModule } from 'ng-zorro-antd/card';
 
 @Component({
   selector: 'profiles',
-  imports: [CommonModule, NzButtonModule, NzIconModule, NzInputModule ,RouterLink, ReactiveFormsModule, DynamicFormComponent],
+  imports: [CommonModule, NzButtonModule, NzIconModule, NzCardModule, RouterLink],
   templateUrl: './profiles.component.html',
   styleUrl: './profiles.component.scss'
 })
 export class ProfilesComponent implements OnInit {
-  protected form: FormGroup;
-  public localStorage: LocalStorageService = inject(LocalStorageService)
+  @ViewChild('editTemplate', { static: true }) editTemplate!: TemplateRef<void>;
+  @ViewChild('deleteTemplate', { static: true }) deleteTemplate!: TemplateRef<void>;
 
-  constructor() {
-    // TODO
-    // refatorar form seguindo esse vídeo: https://www.youtube.com/watch?v=-E-0Gv0yNwY
-    this.form = new FormGroup({
-      name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      description: new FormControl(null, [Validators.required]),
-      links: new FormControl([]),
-      skills: new FormControl([]),
-    })
-  }
+  cardActions: TemplateRef<void>[] = [];
 
   public ngOnInit(): void {
-    // TODO
-    // Verificar se o serviço localStorage não deveria extender Storage
-    const professionalFormData = this.localStorage.get(LocalStorageKeys.professionalFormData);
-    if(professionalFormData) this.form.patchValue(professionalFormData);
-  }
-
-  public submit() {
-    if(this.form.invalid) return;
-    const submittedData = this.form.value;
-    this.localStorage.set(LocalStorageKeys.professionalFormData, submittedData);
+    this.cardActions = [this.editTemplate, this.deleteTemplate];
   }
 }
